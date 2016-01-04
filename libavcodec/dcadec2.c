1d28deef03959487fa7495df3f9c0019a5b012a0
foo86 <foobaz86@gmail.com>
2016-01-04 20:09:36 +0300

    Fix decoding streams without core.

--- libavcodec/dcadec2.c
+++ libavcodec/dcadec2.c
@@ -328,6 +328,22 @@ static int validate_hd_ma_frame(DCA2Context *s)
     return 0;
 }
 
+static int convert_bitstream(const uint8_t *src, int src_size, uint8_t *dst, int max_size)
+{
+    switch (AV_RB32(src)) {
+    case DCA_SYNCWORD_CORE_BE:
+    case DCA_SYNCWORD_SUBSTREAM:
+        memcpy(dst, src, src_size);
+        return src_size;
+    case DCA_SYNCWORD_CORE_LE:
+    case DCA_SYNCWORD_CORE_14B_BE:
+    case DCA_SYNCWORD_CORE_14B_LE:
+        return avpriv_dca_convert_bitstream(src, src_size, dst, max_size);
+    default:
+        return AVERROR_INVALIDDATA;
+    }
+}
+
 static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
                                int *got_frame_ptr, AVPacket *avpkt)
 {
@@ -348,7 +364,7 @@ static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
         return AVERROR(ENOMEM);
 
     for (i = 0, ret = AVERROR_INVALIDDATA; i < input_size - MIN_PACKET_SIZE + 1 && ret < 0; i++)
-        ret = avpriv_dca_convert_bitstream(input + i, input_size - i, s->buffer, s->buffer_size);
+        ret = convert_bitstream(input + i, input_size - i, s->buffer, s->buffer_size);
 
     if (ret < 0)
         return ret;
